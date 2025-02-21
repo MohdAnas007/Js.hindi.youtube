653611025b1953d633a8af887aa126a15ba717ef
MohdAnas007 <gq0222@myamu.ac.in>
2025-02-21 20:36:10 +0000

    changing number to string to  number

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const acountID=1234;
+let accountEmail="Anas@gmail.com";
+var accountPAss="123456";
+accountcity="Sitapur";
+let accountState;
+console.log(acountID);
+console.log("You are not good");
+console.log(accountEmail);
+console.log(accountPAss);
+/*
+dont use var 
+*/
+console.table([accountcity,accountPAss,accountEmail,accountState]);
---
+++ 01_basics/file3.js
@@ -0,0 +1,6 @@
+let score="56";
+console.log(typeof ( score));
+
+// changing string to number 
+let StringToNumber=Number(score);
+console.log(StringToNumber)
