ff37429ae56cfdab06453e1a1ea176666d0536ac
MohdAnas007 <gq0222@myamu.ac.in>
2025-02-23 18:43:02 +0000

    last changes

--- 01_basics/file4.js
+++ 01_basics/file4.js
@@ -1,3 +1,4 @@
 const name="Mohd Anas";
 const email="anas@gmail.com";
 console.log(`My name is ${name} and my email is ${email}.`)
+
---
+++ 01_basics/file5.js
@@ -0,0 +1,7 @@
+// using math library 
+// generating random value
+// math.random gives value in between 0-1 always
+const min=10;
+const max=20;
+let x=Math.floor((Math.random()*(max-min)+ min))
+console.log(x);
