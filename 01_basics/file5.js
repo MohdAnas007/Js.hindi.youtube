b92390a884739b2369f140ac61e3e029d0eee52c
MohdAnas007 <gq0222@myamu.ac.in>
2025-02-23 18:45:23 +0000

    last changesb

--- 01_basics/file5.js
+++ 01_basics/file5.js
@@ -1,6 +1,8 @@
 // using math library 
 // generating random value
 // math.random gives value in between 0-1 always
+// by multiplying by 10 we are changing value to greater than 1
+// by adding min we are giving a min value 
 const min=10;
 const max=20;
 let x=Math.floor((Math.random()*(max-min)+ min))
